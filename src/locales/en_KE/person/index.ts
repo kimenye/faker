9c9bd3cba39fb46a9625e74af360827963c175ba
kimenye <kimenye@gmail.com>
2024-10-08 17:16:51 +0300

    Added generic, first and last names for Kenyan locale

--- src/locales/en_KE/person/index.ts
+++ src/locales/en_KE/person/index.ts
@@ -3,9 +3,15 @@
  * Run 'pnpm run generate:locales' to update.
  */
 import type { PersonDefinition } from '../../..';
+import first_name from './first_name';
+import last_name from './last_name';
+import last_name_pattern from './last_name_pattern';
 import name_ from './name';
 
 const person: PersonDefinition = {
+  first_name,
+  last_name,
+  last_name_pattern,
   name: name_,
 };
 
